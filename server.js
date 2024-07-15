5b683dd7a8c046961ca1bfb62e23da4acd8557d3
eg-delacruz <netoxas3107@hotmail.com>
2024-07-15 15:14:31 +0200

    test: testing CORS error in production

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 import express from 'express';
+import cors from 'cors';
+
 const app = express();
 
 import config from './config.js';
@@ -7,6 +9,7 @@ import http from 'http';
 const server = http.createServer(app);
 
 app.use(express.json());
+app.use(cors());
 
 //Activities data
 import data from './data.js';
@@ -18,7 +21,8 @@ const ACCEPTED_ORIGINS = [
 
 app.get('/', (req, res) => {
   //CORS
-  const origin = req.headers.origin;
+  const origin = req.headers;
+  console.log('Request from: ', origin);
 
   if (ACCEPTED_ORIGINS.includes(origin) || !origin) {
     res.header('Access-Control-Allow-Origin', origin);
@@ -27,13 +31,13 @@ app.get('/', (req, res) => {
   //Securing route with secret key
   const SECRET_KEY = process.env.SERVER_SECRET_KEY;
 
-  // if (req.headers.secret_key === SECRET_KEY) {
-  const randomIndex = Math.floor(Math.random() * data.activities.length);
-  //Return an object that can be seen in the browser
-  res.send(data.activities[randomIndex]);
-  // } else {
-  //res.status(403).json({ error: 'Access denied' });
-  // }
+  if (req.headers.secret_key === SECRET_KEY) {
+    const randomIndex = Math.floor(Math.random() * data.activities.length);
+    //Return an object that can be seen in the browser
+    res.send(data.activities[randomIndex]);
+  } else {
+    res.status(403).json({ error: 'Access denied' });
+  }
 });
 
 server.listen(config.port, () => {
