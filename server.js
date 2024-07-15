56bed268da2fdc5b8ef1b20f0a662a548772267e
eg-delacruz <netoxas3107@hotmail.com>
2024-07-15 15:23:27 +0200

    fix: CORS error in production. Had to use the cors library for express to solve it

--- server.js
+++ server.js
@@ -1,39 +1,29 @@
 import express from 'express';
-import cors from 'cors';
-
-const app = express();
-
 import config from './config.js';
 import http from 'http';
+import cors from 'cors';
+
+//Activities data
+import data from './data.js';
+const app = express();
 
 const server = http.createServer(app);
 
 app.use(express.json());
 app.use(cors());
 
-//Activities data
-import data from './data.js';
-
 //CORS
-const ACCEPTED_ORIGINS = [
-  'https://actividay.vercel.app/, http://localhost:3000/',
-];
+const corsOptions = {
+  origin: ['https://actividay.vercel.app/, http://localhost:3000/'],
+  optionsSuccessStatus: 200,
+};
 
-app.get('/', (req, res) => {
-  //CORS
-  const origin = req.headers;
-  console.log('Request from: ', origin);
-
-  if (ACCEPTED_ORIGINS.includes(origin) || !origin) {
-    res.header('Access-Control-Allow-Origin', origin);
-  }
-
+app.get('/', cors(corsOptions), (req, res) => {
   //Securing route with secret key
   const SECRET_KEY = process.env.SERVER_SECRET_KEY;
 
   if (req.headers.secret_key === SECRET_KEY) {
     const randomIndex = Math.floor(Math.random() * data.activities.length);
-    //Return an object that can be seen in the browser
     res.send(data.activities[randomIndex]);
   } else {
     res.status(403).json({ error: 'Access denied' });
